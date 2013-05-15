95a142fecb30e18cac75b33b88a8822ea18598ae
kallyn_gowdy <2risker@gmail.com>
2013-05-14 22:42:42 -0400

    Parser Classes Created

--- Parser/ParseTable.cs
+++ Parser/ParseTable.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parser.Grammar;
+using Parser;
 
 namespace Parser.StateMachine
 {
     /// <summary>
     /// Defines a parse table.
     /// </summary>
-    public class ParseTable
+    public class ParseTable<T>
     {
+        public Table<int, Terminal<T>, ParseAction<T>> ActionTable;
+
+        public Table<int, NonTerminal<T>, int> GotoTable;
+
     }
 }
---
+++ Parser/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Parser
+    {
+    }
+}
