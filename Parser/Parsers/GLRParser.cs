0bc1216c2bfe392d22fcff9298bbc00f677fb8de
kallyn_gowdy <2risker@gmail.com>
2013-05-28 22:07:34 -0400

    Added GLRParser.

---
+++ Parser/Parsers/GLRParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Parser.Grammar;
+using Parser.Collections;
+using Parser.StateMachine;
+
+namespace Parser.Parsers
+{
+    /// <summary>
+    /// Provides an implementation of a GLR Parser that can parse any table.
+    /// </summary>
+    public class GLRParser<T> : IGrammarParser<T>, IGraphParser<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Gets the parse table that is currently being used.
+        /// </summary>
+        public ParseTable<T> ParseTable
+        {
+            get;
+            private set;
+        }
+
+        public void SetParseTable(StateGraph<GrammarElement<T>, LRItem<T>[]> graph, Terminal<T> endOfInputElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ParseResult<T> ParseAST(IEnumerable<Terminal<T>> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ParseResult<T> ParseSyntaxTree(IEnumerable<Terminal<T>> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParseTable(ContextFreeGrammar<T> grammar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
