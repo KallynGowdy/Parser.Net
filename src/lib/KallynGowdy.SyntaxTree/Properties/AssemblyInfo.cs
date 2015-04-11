11b7f11475d8090e5ce8e4158f3ec28724154ca5
Kallyn Gowdy <kallyngowdy@gmail.com>
2015-04-11 00:28:30 -0400

    Restructure project and convert SyntaxTree projects to PCL

--- src/lib/KallynGowdy.SyntaxTree/Properties/AssemblyInfo.cs
+++ src/lib/KallynGowdy.SyntaxTree/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KallynGowdy.SyntaxTree")]
+[assembly: AssemblyTitle("KallynGowdy.SyntaxTree1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KallynGowdy.SyntaxTree")]
+[assembly: AssemblyProduct("KallynGowdy.SyntaxTree1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("564b08db-f700-4347-a0b1-e80c93e15fc0")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
--- src/test/KallynGowdy.SyntaxTree.Tests/Properties/AssemblyInfo.cs
+++ src/test/KallynGowdy.SyntaxTree.Tests/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KallynGowdy.SyntaxTree.Tests")]
+[assembly: AssemblyTitle("KallynGowdy.SyntaxTree.Tests1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KallynGowdy.SyntaxTree.Tests")]
+[assembly: AssemblyProduct("KallynGowdy.SyntaxTree.Tests1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("61cbeb50-2021-4e95-b52a-cfe9516840e9")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
